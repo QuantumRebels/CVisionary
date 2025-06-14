e5d98974326d52be904ba47f4114a9cd1c3ab69f
Saptarshi Paul <saptarshi2027paul@gmail.com>
2025-06-14 21:37:08 +0530

    Logging Out functionality added

--- frontend/src/pages/dashboard.jsx
+++ frontend/src/pages/dashboard.jsx
@@ -1,10 +1,10 @@
 import { React}  from "react";
+import Navbar from "@/components/landing/Navbar";
 
 const dashboard  = () => {
   return (
     <div className="app">
-      <h1 className="text-red-600 text-6xl font-bold">Welcome to Dashboard Page</h1>
-      <p className="text-3xl text-black font-semibold mt-4">We are currently under construction</p>
+       <Navbar isLoggedIn={true} darkMode={true} setDarkMode={() => {}} />
     </div>
   );
 }
