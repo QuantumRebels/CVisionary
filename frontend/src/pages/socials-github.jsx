e13256bec7da554f310ba5f798aa817481ff312c
Anwesha-B04 <anweshab2020@gmail.com>
2025-06-16 16:34:00 +0530

    added light/dark mode in socials-github page

--- frontend/src/pages/socials-github.jsx
+++ frontend/src/pages/socials-github.jsx
@@ -5,7 +5,6 @@ import { getInitialDarkMode, setDarkModePreference } from "@/utils/theme";
 import Social_Github from "../components/socials/socials-github";
 
 const Social_Github_Connect = () => {
-
   const [darkMode, setDarkMode] = useState(getInitialDarkMode());
 
   const handleSetDarkMode = (value) => {
@@ -13,14 +12,12 @@ const Social_Github_Connect = () => {
     setDarkModePreference(value);
   };
 
-
   return (
     <div className={`app ${darkMode ? "dark" : "light"}`}>
-      <Navbar isLoggedIn={true}  darkMode={darkMode} setDarkMode={handleSetDarkMode}/>
-      <Social_Github />
-    
+      <Navbar isLoggedIn={true} darkMode={darkMode} setDarkMode={handleSetDarkMode} />
+      <Social_Github darkMode={darkMode} />
     </div>
-  )
-}
+  );
+};
 
 export default Social_Github_Connect;
